5dcdd74deba5e52d854d2e6be17574647abb6870
liulanz <zhengliulan@gmail.com>
2020-11-24 16:20:35 -0500

    same dimension rdd

--- similarity.py
+++ similarity.py
@@ -18,21 +18,26 @@ def main():
 
 	# # load text file from local FS
 	rdd = sc.textFile(filename)
-	map1 = rdd.flatMap(lambda x: [(i,(x.split()[0],1)) for i in x.split(" ")[1:]]).groupByKey().mapValues(list)
+	map1 = rdd.flatMap(lambda x: [(i,(x.split()[0],1)) for i in x.split(" ")[1:]]).reduceByKey(lambda x,y : x+y).groupByKey().mapValues(list)
 	terms = map1.map(lambda x: (x[0]))
 	map2 = rdd.flatMap(lambda x: [(x.split()[0], 0)])
 	map3 = terms.cartesian(map2).groupByKey().mapValues(list)
 
 	# map3 = map1.map(lambda x: [(x[0], (x[1].append(('drest',0)))) ])
-
+	map4 = map3.flatMap(lambda x: [((x[0], i[0] ), i[1]) for i in x[1]])
+	map5 = map1.flatMap(lambda x: [((x[0], i[0]),i[1]) for i in x[1]])
+	map6 = map5.union(map4).reduceByKey(lambda x,y : x+y).sortByKey()
+	map7 = map6.map(lambda x: (x[0][0], (x[0][1], x[1]))).groupByKey().mapValues(list)
+	
 
 	# document_number = map1.count()
 	# documentid = map1.map(lambda x: (term ) for term in x[1] for )
 	# map1.join(documentid)
 	# print(documentid.collection())
 
+
 	
-	map3.saveAsTextFile("output/")
+	map7.saveAsTextFile("output/")
 
 if __name__ == '__main__':
 	main()
